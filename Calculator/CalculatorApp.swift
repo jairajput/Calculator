95c75381b648f03711291352df4aadf0b48ac626
jai <jai07rajput@gmail.com>
2022-01-09 11:07:49 +0530

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Jai Vardhan on 09/01/22.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Calculator
+//
+//  Created by Jai Vardhan on 09/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
